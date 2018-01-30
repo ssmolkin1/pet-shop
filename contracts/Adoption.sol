f3af9b0854a7e2ef6b8afe25ee0b117b26d49f33
Sam Smolkin <sam@future-precedent.org>
2018-01-30 09:18:22 +0000

    quick gal commit

---
+++ contracts/Adoption.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.17;
+
+contract Adoption {
+  address[16] public adopters;
+
+  // Adopting a pet
+  function adopt(uint petID) public returns (uint) {
+    require(petID >= 0 && petID <= 15);
+
+    adopters[petID] = msg.sender;
+
+    return petID;
+  }
+
+  
+}
